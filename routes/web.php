72f19ff699422bebb4508d3122de047782c9ad91
Don Ondeje <donondeje@gmail.com>
2021-02-27 10:29:42 +0300

    mileston two preparation

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::get('/', function () {
             ] 
       ]);
       $data = json_decode($response->getBody()->getContents());
-    dd($data->data);
+    dd($data->data->hotelX);
    // return view('welcome',compact('data'));
 });
 
