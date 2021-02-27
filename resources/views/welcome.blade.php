b06a9e8573fffac028330e6995fdd0c6baf2d2d3
Don Ondeje <donondeje@gmail.com>
2021-02-27 12:15:45 +0300

    mileston two preparation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -556,8 +556,8 @@ function googleTranslateElementInit() {
                         <label style="position:relative;top:14px;left:13px;z-index: 111111;">Hotel Name</label>
                         <!--<h6>Location or Hotel Name</h6>  <input type="text" id="location" class="form-control" name="city" placeholder="Location or Hotel Name" style="border:0;padding-top:12px !important;height:57px">-->
                     <select class="form-control" name="venues[]" style="width:100%;">
-                      @foreach($destinations as $destination => $values)
-                    <option value="{!! $values->node->destinationData->texts['text'] !!}">{!! $values->node->destinationData->texts['text'] !!}</option>
+                        @foreach($destinations as $destination => $values)
+                    <option value="{!! $values->node->destinationData->texts[0]->text !!}">{!! $values->node->destinationData->texts[0]->text !!}</option>
                         @endforeach
                     </select>
                     <div class="hot-loc-div input-field-div" style="display:none">
