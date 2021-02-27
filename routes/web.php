84ee216c3b9888961d4f6c2cfc13bd024259a5f9
Don Ondeje <donondeje@gmail.com>
2021-02-27 09:55:04 +0300

    mileston three

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::get('/', function () {
             ] 
       ]);
       $data = json_decode($response->getBody()->getContents());
-    // dd($data);
+    dd($data);
     //return view('welcome');
 });
 
