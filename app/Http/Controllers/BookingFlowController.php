2ab67fea31453acc7b44e87e22d7084ca6cd1d52
DonOndeje <donondeje@gmail.com>
2021-03-08 19:23:08 +0530

    added dynamic destinations

--- app/Http/Controllers/BookingFlowController.php
+++ app/Http/Controllers/BookingFlowController.php
@@ -279,8 +279,8 @@ class BookingFlowController extends Controller
         $options = $data['data']['hotelX']['search']['options'];
 
          for($i=0;$i<count($options);$i++){
-          if($options[$i]['hotelCode'] == $options[($i++)]['hotelCode']){
-             unset($options[($i++)]);
+          if($options[$i]['hotelCode'] == $options[(++$i)]['hotelCode']){
+             unset($options[(++$i)]);
           }
          }
 
