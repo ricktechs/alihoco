fbd535dcebd60d75eb6467e8f64d3fc71f1a3407
DonOndeje <donondeje@gmail.com>
2021-03-08 13:10:42 +0530

    added dynamic destinations

--- app/Http/Controllers/BookingFlowController.php
+++ app/Http/Controllers/BookingFlowController.php
@@ -16,7 +16,6 @@ class BookingFlowController extends Controller
         $checkin = $request->input('checkin');
         $checkout = $request->input('checkout');
         $destinations = $request->input('location');
-        dd($destinations);
         $client = new \GuzzleHttp\Client();
         $response =   $client->request('POST', 'https://api.travelgatex.com/', [ 
           'headers' => [
@@ -25,219 +24,259 @@ class BookingFlowController extends Controller
             'json' => [
                 'query' => ' 
                 {
-                  hotelX { 
-                    search(criteria: {checkIn: "'.$checkin.'", checkOut: "'.$checkout.'",destinations:["016880"]
-                    occupancies: [{paxes: [{age: 30}, {age: 30}]}], language: "es", nationality: "ES", currency: "EUR", market: "ES"}, settings: {suppliers:{code: "MTBK"}, 
-                    plugins: {step: REQUEST, pluginsType: [{type: POST_STEP, name: "search_by_destination", 
-                    parameters: [{key: "accessID", value: "8394"}]}]}, businessRules: null, timeout: 24700, useContext:true,
-                    context: "MTBK", client: "allinclusive", testMode: false}, filter: {access: {includes: ["8394"]}}) {
-                      context
-                      warnings {
-                        code
-                        type
-                        description
-                      }
-                      options {
-                        surcharges {
-                          chargeType
-                          mandatory
-                          description
-                          price {
-                            currency
-                            binding
-                            net
-                            gross
-                            exchange {
-                              currency
-                              rate
-                            }
-                            markups {
-                              channel
-                              currency
-                              binding
-                              net
-                              gross
-                              exchange {
-                                currency
-                                rate
-                              }
-                            }
-                          }
-                        }
-                        accessCode
-                        supplierCode
-                        hotelCode
-                        hotelName
-                        boardCode
-                        paymentType
-                        status
-                        
-                        occupancies {
-                          id
-                          paxes {
-                            age
-                          }
-                        }
-                        rooms {
-                          occupancyRefId
+                  hotelX{
+                    destinations(criteria:{access:"8394",destinationCodes:['.$destinations.']},relay:{},){
+                     token
+                      edges{
+                        node{
                           code
-                          description
-                          refundable
-                          units
-                          roomPrice {
-                            price {
-                              currency
-                              binding
-                              net
-                              gross
-                              exchange {
-                                currency
-                                rate
-                              }
-                              markups {
-                                channel
-                                currency
-                                binding
-                                net
-                                gross
-                                exchange {
-                                  currency
-                                  rate
-                                }
-                              }
-                            }
-                          }
-                          beds {
+                          destinationData{
+                            destinationLeaf
+                            available
+                            code
                             type
-                           
-                            count
-                            shared
+                            parent
+                            texts{
+                              language
+                              text
+                              
+                            }
                           }
-                          ratePlans {
-                            code
-                            name
-                            
-                          }
-                          promotions {
-                            code
-                            name
                           
-                          }
                         }
-                        price {
-                          currency
-                          binding
-                          net
-                          gross
-                          exchange {
-                            currency
-                            rate
-                          }
-                          markups {
-                            channel
-                            currency
-                            binding
-                            net
-                            gross
-                            exchange {
-                              currency
-                              rate
-                            }
-                          }
-                        }
-                        supplements {
-                          code
-                          name
-                          description
-                          supplementType
-                          chargeType
-                          mandatory
-                          durationType
-                          quantity
-                          unit
-                          resort {
-                            code
-                            name
-                            description
-                          }
-                          price {
-                            currency
-                            binding
-                            net
-                            gross
-                            exchange {
-                              currency
-                              rate
-                            }
-                            markups {
-                              channel
-                              currency
-                              binding
-                              net
-                              gross
-                              exchange {
-                                currency
-                                rate
-                              }
-                            }
-                          }
-                        }
-                        surcharges {
-                          chargeType
-                          description
-                          price {
-                            currency
-                            binding
-                            net
-                            gross
-                            exchange {
-                              currency
-                              rate
-                            }
-                            markups {
-                              channel
-                              currency
-                              binding
-                              gross
-                              exchange {
-                                currency
-                                rate
-                              }
-                            }
-                          }
-                        }
-                        rateRules
-                        cancelPolicy {
-                          refundable
-                          cancelPenalties {
-                            hoursBefore
-                            penaltyType
-                            currency
-                            value
-                          }
-                        }
-                        remarks
-                        id
                       }
-                      errors {
-                        code
-                        type
-                        description
-                      }
-                      warnings {
-                        code
-                        type
-                        description
-                      }
+                      
                     }
                   }
                 }
                 '
               ] 
         ]);
+        
+
+
+
+
+        // $response =   $client->request('POST', 'https://api.travelgatex.com/', [ 
+        //   'headers' => [
+        //     'Authorization' => 'Apikey 97953250-6dde-489b-67e6-f781510f78bf'
+        //   ], 
+        //     'json' => [
+        //         'query' => ' 
+        //         {
+        //           hotelX { 
+        //             search(criteria: {checkIn: "'.$checkin.'", checkOut: "'.$checkout.'",destinations:["016880"]
+        //             occupancies: [{paxes: [{age: 30}, {age: 30}]}], language: "es", nationality: "ES", currency: "EUR", market: "ES"}, settings: {suppliers:{code: "MTBK"}, 
+        //             plugins: {step: REQUEST, pluginsType: [{type: POST_STEP, name: "search_by_destination", 
+        //             parameters: [{key: "accessID", value: "8394"}]}]}, businessRules: null, timeout: 24700, useContext:true,
+        //             context: "MTBK", client: "allinclusive", testMode: false}, filter: {access: {includes: ["8394"]}}) {
+        //               context
+        //               warnings {
+        //                 code
+        //                 type
+        //                 description
+        //               }
+        //               options {
+        //                 surcharges {
+        //                   chargeType
+        //                   mandatory
+        //                   description
+        //                   price {
+        //                     currency
+        //                     binding
+        //                     net
+        //                     gross
+        //                     exchange {
+        //                       currency
+        //                       rate
+        //                     }
+        //                     markups {
+        //                       channel
+        //                       currency
+        //                       binding
+        //                       net
+        //                       gross
+        //                       exchange {
+        //                         currency
+        //                         rate
+        //                       }
+        //                     }
+        //                   }
+        //                 }
+        //                 accessCode
+        //                 supplierCode
+        //                 hotelCode
+        //                 hotelName
+        //                 boardCode
+        //                 paymentType
+        //                 status
+                        
+        //                 occupancies {
+        //                   id
+        //                   paxes {
+        //                     age
+        //                   }
+        //                 }
+        //                 rooms {
+        //                   occupancyRefId
+        //                   code
+        //                   description
+        //                   refundable
+        //                   units
+        //                   roomPrice {
+        //                     price {
+        //                       currency
+        //                       binding
+        //                       net
+        //                       gross
+        //                       exchange {
+        //                         currency
+        //                         rate
+        //                       }
+        //                       markups {
+        //                         channel
+        //                         currency
+        //                         binding
+        //                         net
+        //                         gross
+        //                         exchange {
+        //                           currency
+        //                           rate
+        //                         }
+        //                       }
+        //                     }
+        //                   }
+        //                   beds {
+        //                     type
+                           
+        //                     count
+        //                     shared
+        //                   }
+        //                   ratePlans {
+        //                     code
+        //                     name
+                            
+        //                   }
+        //                   promotions {
+        //                     code
+        //                     name
+                          
+        //                   }
+        //                 }
+        //                 price {
+        //                   currency
+        //                   binding
+        //                   net
+        //                   gross
+        //                   exchange {
+        //                     currency
+        //                     rate
+        //                   }
+        //                   markups {
+        //                     channel
+        //                     currency
+        //                     binding
+        //                     net
+        //                     gross
+        //                     exchange {
+        //                       currency
+        //                       rate
+        //                     }
+        //                   }
+        //                 }
+        //                 supplements {
+        //                   code
+        //                   name
+        //                   description
+        //                   supplementType
+        //                   chargeType
+        //                   mandatory
+        //                   durationType
+        //                   quantity
+        //                   unit
+        //                   resort {
+        //                     code
+        //                     name
+        //                     description
+        //                   }
+        //                   price {
+        //                     currency
+        //                     binding
+        //                     net
+        //                     gross
+        //                     exchange {
+        //                       currency
+        //                       rate
+        //                     }
+        //                     markups {
+        //                       channel
+        //                       currency
+        //                       binding
+        //                       net
+        //                       gross
+        //                       exchange {
+        //                         currency
+        //                         rate
+        //                       }
+        //                     }
+        //                   }
+        //                 }
+        //                 surcharges {
+        //                   chargeType
+        //                   description
+        //                   price {
+        //                     currency
+        //                     binding
+        //                     net
+        //                     gross
+        //                     exchange {
+        //                       currency
+        //                       rate
+        //                     }
+        //                     markups {
+        //                       channel
+        //                       currency
+        //                       binding
+        //                       gross
+        //                       exchange {
+        //                         currency
+        //                         rate
+        //                       }
+        //                     }
+        //                   }
+        //                 }
+        //                 rateRules
+        //                 cancelPolicy {
+        //                   refundable
+        //                   cancelPenalties {
+        //                     hoursBefore
+        //                     penaltyType
+        //                     currency
+        //                     value
+        //                   }
+        //                 }
+        //                 remarks
+        //                 id
+        //               }
+        //               errors {
+        //                 code
+        //                 type
+        //                 description
+        //               }
+        //               warnings {
+        //                 code
+        //                 type
+        //                 description
+        //               }
+        //             }
+        //           }
+        //         }
+        //         '
+        //       ] 
+        // ]);
         $data = json_decode($response->getBody()->getContents(), true);
-        $options = $data['data']['hotelX']['search']['options'];
-       //dd($options);
+       // $options = $data['data']['hotelX']['search']['options'];
+       dd($data);
       return view('results', compact('options'));
     }
 }
